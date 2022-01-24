c318eab063b9902ff6d3ba41c99d6b0d2d36d904
alialaba <alialaba079@gmail.com>
2022-01-24 10:27:16 -0800

    added a global state(data structure)

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "School Meeting",
+      day: "Jan 11th at 12pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Appointment with Doctor",
+      day: "Jan 12th at 3pm",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "School Meeting",
+      day: "Jan 15th at 11am",
+      reminder: false
+    }
+  ])
   return (
     <div className="container">
       <Header />
