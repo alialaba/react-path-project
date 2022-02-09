922dc7f13201a97629273e69ac135df0cf36490f
alialaba <alialaba079@gmail.com>
2022-02-09 09:19:16 -0800

    chore:install json server

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -5,26 +5,7 @@ import AddNew from "./components/AddNew";
 
 const App = () => {
   const [showOnAdd, setShowOnAdd] = useState(false);
-  const [tasks, setTasks] = useState([
-    // {
-    //   id: 1,
-    //   text: "School Meeting",
-    //   day: "Jan 11th at 12pm",
-    //   reminder: true
-    // },
-    // {
-    //   id: 2,
-    //   text: "Appointment with Doctor",
-    //   day: "Jan 12th at 3pm",
-    //   reminder: true
-    // },
-    // {
-    //   id: 3,
-    //   text: "School Meeting",
-    //   day: "Jan 15th at 11am",
-    //   reminder: false
-    // }
-  ])
+  const [tasks, setTasks] = useState([])
 
   //add task
 
