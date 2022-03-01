1c6b77ba92627850e737c5c57f1b79f2e5ee6c4e
alialaba <alialaba079@gmail.com>
2022-03-01 09:16:53 -0800

    style hero section by adding padding/margin

--- airbnb-experience/src/components/HeroSection.js
+++ airbnb-experience/src/components/HeroSection.js
@@ -3,13 +3,14 @@ const HeroSection = () => {
         <section>
             <div className="container">
                 <div className="hero">
+                    <img src={process.env.PUBLIC_URL + "/images/photo-grid.png"} />
 
-                </div>
-                <div className="hero-content">
-                    <h1>Online Experiences</h1>
-                    <p>
-                        Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-                    </p>
+                    <div className="hero-content">
+                        <h1 className="title">Online Experiences</h1>
+                        <p className="text">
+                            Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
