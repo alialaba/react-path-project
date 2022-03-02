17b7344de539f9e51c2bb950ca52a093b42bab30
alialaba <alialaba079@gmail.com>
2022-03-02 09:27:59 -0800

    added padding to space around the nav

--- travel-journal/src/components/Navbar.js
+++ travel-journal/src/components/Navbar.js
@@ -1,8 +1,9 @@
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className="nav">
+            <img className="nav__logo" src={process.env.PUBLIC_URL + "/logo.png"} />
+            <p className="nav__text">my travel journal</p>
+        </nav>
     );
 }
 export default Navbar
