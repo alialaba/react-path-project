21ea592cff0238a43dd9a5bd7327974c31d09383
alialaba <alialaba079@gmail.com>
2022-03-25 11:02:40 -0700

    implemented the sidebar note add functionality

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -11,15 +11,31 @@ const App = () => {
     (notes[0] && notes[0].id) || ""
   )
 
+  //function that create new note obj
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      type: "# Type your markdown note's title here "
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id)
+  }
+
   return (
     <main>
       <Split
-        sizes={[30, 70]}
+        sizes={[20, 80]}
         direction="horizontal"
         className="split"
       >
-        <Sidebar />
+        <Sidebar
+          notes={notes}
+          newNote={createNewNote}
 
+        />
+        <Editor
+          newNote={createNewNote}
+        />
 
       </Split>
 
--- notes-app/src/components/Sidebar.js
+++ notes-app/src/components/Sidebar.js
@@ -1,8 +1,19 @@
 const Sidebar = ({ notes, newNote }) => {
 
+    const noteElements = notes.map((note, index) =>
+        <div key={note.id}>
+            <div className="title">
+                <h4>Note {index + 1}</h4>
+            </div>
+        </div>)
+
     return (
         <aside className="pane sidebar" >
-            sidebar
+            <div className="sidebar__header">
+                <h4 className="sidebar__title">NoteApp</h4>
+                <button className="sidebar__btn" onClick={newNote} >+</button>
+            </div>
+            {noteElements}
         </ aside>
     );
 }
