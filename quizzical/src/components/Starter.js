133e01c8eb061c1be9ed27d7632681c03ea8f370
alialaba <alialaba079@gmail.com>
2022-05-03 10:54:59 -0700

    added started files for quizzical

---
+++ quizzical/src/components/Starter.js
@@ -0,0 +1,10 @@
+const Starter = () => {
+    return (
+        <div className="wrap">
+            <h1>Quizzical</h1>
+            <p>Some description if needed</p>
+            <a className="btn" href="#">Start quiz</a>
+        </div>
+    )
+}
+export default Starter;
